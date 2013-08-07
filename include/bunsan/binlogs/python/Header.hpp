ea097c773bd8b115986936fede6b5e39b0fa112a
Aleksey Filippov <sarum9in@gmail.com>
2013-08-07 21:03:32 +0400

    Header refactoring + bunsan_binlogs -> _binlogs.

--- include/bunsan/binlogs/python/Header.hpp
+++ include/bunsan/binlogs/python/Header.hpp
@@ -3,16 +3,19 @@
 #include "bunsan/binlogs/Header.hpp"
 
 #include <string>
+#include <vector>
 
 namespace bunsan {
 namespace binlogs {
 namespace python {
 
+typedef std::vector<std::string> StringList;
+
 struct Header {
     static Header get(const binlogs::Header &header);
 
     std::string proto;
-    std::vector<std::string> types;
+    StringList types;
 };
 
 }
--- src/python/binlogs.cpp
+++ src/python/binlogs.cpp
@@ -1,12 +1,16 @@
 #include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
 #include "bunsan/binlogs/python/Header.hpp"
 
 using namespace boost::python;
 namespace binlogs = bunsan::binlogs;
 
-BOOST_PYTHON_MODULE(bunsan_binlogs)
+BOOST_PYTHON_MODULE(_binlogs)
 {
+    class_<binlogs::python::StringList>("StringList")
+        .def(vector_indexing_suite<binlogs::python::StringList>());
+
     class_<binlogs::python::Header>("Header", no_init)
         .def_readonly("proto", &binlogs::python::Header::proto)
         .def_readonly("types", &binlogs::python::Header::types);
